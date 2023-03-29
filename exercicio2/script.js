12b2d3c7d0c9f22f1f23cc2bdbdd44cf9ad3ed81
leosilvapb <leo-leonardo-silva@hotmail.com>
2023-03-29 15:35:13 -0300

    resolução dos exercicios

---
+++ exercicio2/script.js
@@ -0,0 +1,18 @@
+const num = Number(prompt("Informe um número"))
+let i = 1
+
+while (i <= 10){
+
+    console.log(`${num} x ${i} = ${num * i}`)
+    i++
+}
+
+
+console.log("===============================================")
+
+
+for(i = 1; i <= 10; i++){
+
+    console.log(`${num} x ${i} = ${num * i}`)
+
+}
---
+++ exercicio3/script.js
@@ -0,0 +1,18 @@
+const maioresPaises = ["Rússia", "Canadá", "China", "EUA", "Brasil"]
+let i = 0
+
+for (let i = 0; i < maioresPaises.length; i++) {
+
+    console.log(`"${i + 1} - ${maioresPaises[i]}"`)
+
+}
+
+console.log("===============================================")
+
+
+while (i < maioresPaises.length) {
+
+    console.log(`"${i + 1} - ${maioresPaises[i]}"`)
+
+    i++
+}
